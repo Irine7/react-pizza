04f26e2893f54ad1dcf092857509a1e9ffeb0636
Irine7 <irine7@yandex.ru>
2023-09-08 13:12:08 +0200

    feat: skeleton preloading logics

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import React from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 import './scss/app.scss';
-import pizzas from './assets/pizzas.json';
 
 function App() {
+	const [pizzas, setPizzas] = React.useState([]);
+	const [loading, setLoading] = React.useState(true);
+	const apiUrl = 'https://64f9d6b84098a7f2fc150eaf.mockapi.io/pizzas';
+
+	// useEffect calls API just once. [] means the component will be mounted only once
+	React.useEffect(() => {
+		fetch(apiUrl)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setPizzas(data);
+				setLoading(false);
+			});
+	}, []);
+
 	return (
 		<div className="wrapper">
 			<Header />
@@ -20,9 +36,9 @@ function App() {
 					</div>
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
-						{pizzas.map((pizza) => (
-							<PizzaBlock key={pizza.id} {...pizza}/>
-						))}
+						{loading
+							? [... new Array(12)].map((_, index) => <Skeleton key={index} />)
+							: pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
 					</div>
 				</div>
 			</div>
