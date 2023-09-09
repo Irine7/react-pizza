e99380972e16cf21154c2282a09e91a5301db870
Irine7 <irine7@yandex.ru>
2023-09-09 16:47:30 +0200

    feat: add NotFound & Cart

---
+++ src/components/NotFoundPart/NotFoundPart.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './NotFoundPart.module.scss';
+console.log(styles);
+
+const NotFoundPart = () => {
+	return (
+		<div className={styles.root}>
+			<h1>
+				<span>🤷‍♀️</span>
+				<br />
+				Nothing found
+			</h1>
+			<p className={styles.description}>Unfortunately, nothing matched your search terms</p>
+		</div>
+	);
+};
+
+export default NotFoundPart;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import NotFoundPart from '../components/NotFoundPart/NotFoundPart';
+
+const NotFound = () => {
+	return <NotFoundPart />;
+};
+
+export default NotFound;
